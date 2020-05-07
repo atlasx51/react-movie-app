4b46b71c5e93ba133221694ce90ac4b8b928204c
Eric V <ericvarg84@gmail.com>
2020-05-06 21:02:44 -0700

    first time

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import Search from './components/Search'
+import axios from 'axios'
 
 function App() {
+  const [state, setState]= useState({
+    s: '',
+    results:[],
+    selected: {}
+  })
+  const apiurl ='http://www.omdbapi.com/?apikey=c91ed5'
+
+  const search = (e)=>{
+    if(e.key === 'Enter'){
+      axios(apiurl +'&s=' + state.s).then(({data})=>{
+        let results = data.search
+        
+        setState =(prevState =>{
+          return{...prevState, results:results}
+        })
+         
+      })
+    }
+  }
+
+  const handleInput = (e)=> {
+    let s = e.target.value
+
+    setState(prevState =>{
+      return{...prevState, s:s}
+    })
+
+    console.log(state.s)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>Movie Database</h1>
       </header>
+      <main>
+        <Search handleInput={handleInput} search={search}/>
+      </main>
     </div>
   );
 }
